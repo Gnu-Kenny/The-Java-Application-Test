e029b34968d9670aa8b9b1568a1d151165e1daa6
Gnu-Kenny <qkr0677@gmail.com>
2022-03-29 15:51:15 +0900

    Assumptions 내용 정리
    조건에 따라 테스트 실행하기
    - assumeThat(조건)
    - assumingThat(조건, 테스트)
    - EnabledOnOS, DisabledOnOS

--- src/test/java/com/example/thejavatest/StudyTest.java
+++ src/test/java/com/example/thejavatest/StudyTest.java
@@ -1,27 +1,46 @@
 package com.example.thejavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
+
+import static org.hamcrest.Matchers.*;
 
 import java.time.Duration;
+import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 class StudyTest {
     @Test
     @DisplayName("스터디 만들기")
+    @EnabledOnJre({JRE.JAVA_8, JRE.JAVA_11})
+    @DisabledOnOs(value = {OS.MAC})
     void create_new_study(){
-        Study study = new Study(10);
-        // assertAll 테스트하고자하는 것들을 묶어 앞단의 테스트가 실패하더라도 이후 테스트를 계속 수행할 수 있게 하는 함수
-        assertAll(
-                () -> assertNotNull(study),
-                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 DRAFT 상태다."),
-                () -> assertTrue(study.getLimit() < 0, "limit은 0보다 더 커야한다.")
-        );
+        // 조건문
+        String test_env = System.getenv("TEST_ENV");
+
+        assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+
+        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+            System.out.println("LOCAL");
+            Study actual = new Study(10);
+            assertThat(actual.getLimit(), is(greaterThan(0)));
+        });
+        assumingThat("GLOBAL".equalsIgnoreCase(test_env), () -> {
+            System.out.println("GLOBAL");
+            Study actual = new Study(100);
+            assertThat(actual.getLimit(), is(greaterThan(0)));
+        });
+        System.out.println("test successfully");
     }
 
     @Test
+    @DisabledOnOs(OS.OTHER)
     @DisplayName("스터디 다시 만들기")
     void create_new_study_again() {
+        System.out.println(System.getProperty("os.name").toLowerCase());
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
         assertEquals("limit은 0보다 더 커야한다.", exception.getMessage());
     }
@@ -38,24 +57,24 @@ class StudyTest {
         //              트랙잭션 설정이 테스트에서 적용이 제대로 이루어지지 않는다면 롤백되어야 할 상황에서 디비 반영이 되는 에러가 발생할 수 있다.
         //              -> 트랙잭션 설정을 가지고 있는 코드와 별개의 스레드로 위의 테스트 코드를 실행하게 되기 때문
     }
-
-    @BeforeAll // 테스트전 딱 한번, 반드시 static를 붙여야함
-    static void beforeAll() {
-        System.out.println("before all");
-    }
-
-    @AfterAll
-    static void afterAll() {
-        System.out.println("after all");
-    }
-
-    @BeforeEach
-    void beforeEach() {
-        System.out.println("beforeEach");
-    }
-
-    @AfterEach
-    void afterEach() {
-        System.out.println("afterEach");
-    }
+//
+//    @BeforeAll // 테스트전 딱 한번, 반드시 static를 붙여야함
+//    static void beforeAll() {
+//        System.out.println("before all");
+//    }
+//
+//    @AfterAll
+//    static void afterAll() {
+//        System.out.println("after all");
+//    }
+//
+//    @BeforeEach
+//    void beforeEach() {
+//        System.out.println("beforeEach");
+//    }
+//
+//    @AfterEach
+//    void afterEach() {
+//        System.out.println("afterEach");
+//    }
 }
